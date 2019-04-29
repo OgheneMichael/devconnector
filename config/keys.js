48ec2de63e24a2351fec74af673b23b2d0221bca
OgheneMichael <zonalmeet@gmail.com>
2019-04-29 23:58:55 +0100

    Created the JWT

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
 	mongoURI:
-		"mongodb+srv://oghene:oghene@cluster0-dhnyj.mongodb.net/test?retryWrites=true"
+		"mongodb+srv://oghene:oghene@cluster0-dhnyj.mongodb.net/test?retryWrites=true",
+	secretOrKey: "secret"
 };
