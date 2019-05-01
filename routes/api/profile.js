c7b6d004fc0db6d98839da0973703a2ceed51e7d
OgheneMichael <zonalmeet@gmail.com>
2019-05-01 10:09:01 +0100

    Added profile validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+// Load Validation;
+const validateProfileInput = require("../../validation/profile");
+
 // Load Profile Model
 const Profile = require("../../models/Profile");
 
@@ -24,6 +27,7 @@ router.get(
 		const errors = {};
 
 		Profile.findOne({ user: req.user.id })
+			.populate("user", ["name", "avatar"])
 			.then(profile => {
 				if (!profile) {
 					errors.noprofile = "There is no profile for this user";
@@ -42,9 +46,19 @@ router.post(
 	"/",
 	passport.authenticate("jwt", { session: false }),
 	(req, res) => {
+		const { errors, isValid } = validateProfileInput(req.body);
+
+		// Check validation
+
+		if (!isValid) {
+			// Return any errors with 400 status
+			return res.status(400).json(errors);
+		}
+
 		// Get fields
 		const profileFields = {};
 		profileFields.user = req.user.id;
+		if (req.body.handle) profileFields.handle = req.body.handle;
 		if (req.body.company) profileFields.company = req.body.company;
 		if (req.body.website) profileFields.website = req.body.website;
 		if (req.body.location) profileFields.location = req.body.location;
@@ -86,17 +100,13 @@ router.post(
 					}
 
 					// Save Profile
-					new Profile(profileFields).save().then(profile => res.json(profile));
+					new Profile(profileFields)
+						.save()
+						.then(profile => res.json(profile))
+						.catch(err => res.status(400).json(err));
 				});
 			}
 		});
-
-		if (req.body.handle) profileFields.handle = req.body.handle;
-		if (req.body.handle) profileFields.handle = req.body.handle;
-		if (req.body.handle) profileFields.handle = req.body.handle;
-		if (req.body.handle) profileFields.handle = req.body.handle;
-		if (req.body.handle) profileFields.handle = req.body.handle;
-		if (req.body.handle) profileFields.handle = req.body.handle;
 	}
 );
 
