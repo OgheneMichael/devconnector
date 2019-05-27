1148fde5ebd9d499b8b40b396bc8514f5cde630d
OgheneMichael <zonalmeet@gmail.com>
2019-05-27 15:13:15 +0100

    Created PrivateRoute component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { clearCurrentProfile } from "./actions/profileActions";
+
+import PrivateRoute from "./components/common/PrivateRoute";
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -50,7 +52,9 @@ function App() {
 					<div className="container">
 						<Route exact path="/register" component={Register} />
 						<Route exact path="/login" component={Login} />
-						<Route exact path="/dashboard" component={Dashboard} />
+						<Switch>
+							<PrivateRoute exact path="/dashboard" component={Dashboard} />
+						</Switch>
 					</div>
 					<Footer />
 				</div>
