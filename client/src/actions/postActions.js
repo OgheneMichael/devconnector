9706e44d2616f24a5c8d035506c0daf08ebaf5fc
OgheneMichael <zonalmeet@gmail.com>
2019-07-13 09:49:15 +0100

    Built single post page

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import {
 	ADD_POST,
 	GET_POSTS,
+	GET_POST,
 	GET_ERRORS,
 	POST_LOADING,
 	DELETE_POST
@@ -85,6 +86,25 @@ export const removeLike = id => dispatch => {
 			dispatch({
 				type: GET_ERRORS,
 				payload: err.response.data
+			})
+		);
+};
+
+// Get Post
+export const getPost = id => dispatch => {
+	dispatch(setPostLoading);
+	axios
+		.get(`/api/posts/${id}`)
+		.then(res =>
+			dispatch({
+				type: GET_POST,
+				payload: res.data
+			})
+		)
+		.catch(err =>
+			dispatch({
+				type: GET_POST,
+				payload: null
 			})
 		);
 };
